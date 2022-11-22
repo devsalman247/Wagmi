79e28f141c065fcc82d2789de3a88a3816586397
devsalman247 <salman247.dev@gmail.com>
2022-11-22 20:11:05 +0500

    some fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -41,7 +41,6 @@ function HomePage() {
   // useBalance()
   const { data, isError } = useBalance({
     address,
-    token: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
     onSuccess(data) {
       console.log("Success", data);
     },
