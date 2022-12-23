33636a9e349d7857fa328c940a0985f007901688
devsalman247 <salman247.dev@gmail.com>
2022-12-24 03:25:06 +0500

    updated wagmi packes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
-import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
-} from "@web3modal/ethereum";
+import { EthereumClient, modalConnectors, walletConnectProvider } from "@web3modal/ethereum";
 
 import { Web3Modal } from "@web3modal/react";
 
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { avalanche, bsc, mainnet, bscTestnet } from "wagmi/chains";
 
 import HomePage from "./Home";
 
 function App() {
-  
-  const chains = [chain.mainnet, chain.polygon, chain.optimism, chain.goerli, chain.arbitrum];
+	const chains = [mainnet, avalanche, bsc, bscTestnet];
+	const { provider } = configureChains(chains, [
+		walletConnectProvider({ projectId: "f3cd5988a9d89b94b82b627a339f6e3d" }),
+	]);
 
-  const { provider } = configureChains(chains, [
-    walletConnectProvider({ projectId: "f3cd5988a9d89b94b82b627a339f6e3d" }),
-  ]);
+	const wagmiClient = createClient({
+		autoConnect: true,
+		connectors: modalConnectors({ appName: "web3Modal", chains }),
+		provider,
+	});
+	const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-  const wagmiClient = createClient({
-    autoConnect : true,
-    connectors : modalConnectors({ appName: "web3Modal", chains }),
-    provider,
-  });
+	return (
+		<>
+			<WagmiConfig client={wagmiClient}>
+				<HomePage />
+			</WagmiConfig>
 
-  const ethereumClient = new EthereumClient(wagmiClient, chains);
-
-  return (
-    <>
-      <WagmiConfig client={wagmiClient}>
-        <HomePage />
-      </WagmiConfig>
-
-      <Web3Modal
-        projectId="f3cd5988a9d89b94b82b627a339f6e3d"
-        theme="dark"
-        accentColor="default"
-        ethereumClient={ethereumClient}
-      />
-    </>
-  );
+			<Web3Modal
+				projectId="f3cd5988a9d89b94b82b627a339f6e3d"
+				theme="dark"
+				accentColor="default"
+				ethereumClient={ethereumClient}
+			/>
+		</>
+	);
 }
 
 export default App;
